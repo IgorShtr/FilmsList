8a053a045585fe28d0a857544c1975502abf84a8
Igor Shtr <igor.shtr@gmail.com>
2020-05-02 14:41:06 +0300

    Updates

---
+++ precache-manifest.657a7fceea9c1b8e4c549d0de39ecbe3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87c56cc8e65d11337756b035ba1f0c3",
+    "url": "/filmsList/index.html"
+  },
+  {
+    "revision": "4c98cf3d72f6f6d3cf4c",
+    "url": "/filmsList/static/css/main.70b58b71.chunk.css"
+  },
+  {
+    "revision": "1c1ea6a11321a28d5345",
+    "url": "/filmsList/static/js/2.905e660e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/filmsList/static/js/2.905e660e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c98cf3d72f6f6d3cf4c",
+    "url": "/filmsList/static/js/main.e2c794cf.chunk.js"
+  },
+  {
+    "revision": "b90449d5047d3747374b",
+    "url": "/filmsList/static/js/runtime-main.5306f8b9.js"
+  }
+]);
